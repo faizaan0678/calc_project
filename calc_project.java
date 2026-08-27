4a6c08ac1d6ac4604dc40bc08d7e54f96dbc4c09
faizaan0678 <faizaanm046@gmail.com>
2026-08-27 15:27:10 +0530

    Correct add method and main method syntax
    Fix syntax errors and improve the add method.

--- calc_project.java
+++ calc_project.java
@@ -1,7 +1,11 @@
 class calc_project{
   public int add(int a,int b){
+    int c=a+b;
+    return c;
   }
   public static void main(String[] args){
+    calc_project cal=new calc_project();
+    system.out.println("THE SUM IS:"+CAL.ADD(2,4));
     {
     }
   }
