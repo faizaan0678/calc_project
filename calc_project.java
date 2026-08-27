f30f2c8e42a9d92bbc12bf6d2799ba616a05b411
faizaan0678 <faizaanm046@gmail.com>
2026-08-27 15:29:19 +0530

    Add subtraction method and correct print statements
    Added subtraction method and fixed output statements.

--- calc_project.java
+++ calc_project.java
@@ -2,10 +2,14 @@ class calc_project{
   public int add(int a,int b){
     int c=a+b;
     return c;
+    public int sub(int a,int b0{
+      int c=a-b;
+      return c;
   }
   public static void main(String[] args){
     calc_project cal=new calc_project();
     system.out.println("THE SUM IS:"+CAL.ADD(2,4));
+    system.out.println("THE SUBSTRACTION IS:"+CAL.ADD(2,4));
     {
     }
   }
